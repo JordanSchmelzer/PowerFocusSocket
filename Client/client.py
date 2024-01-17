1d4da1c37a19cd4489f3bb68d4d387838329a323
Jordan Schmelzer <jordanschmelzer@gmail.com>
2024-01-17 00:04:14 -0600

    mid 1,3 written

--- Client/client.py
+++ Client/client.py
@@ -24,6 +24,43 @@ def send(msg):
     print(client.recv(2048).decode(FORMAT))
 
 
+def buildMidMessage(mid, revision, data):
+    message_end = "NUL"
+
+    # data
+    # data field (no data for this)
+
+    # Build header
+    mid = "0001"
+    revision = "003"
+    noAckFlag = " " * 1
+    stationId = " " * 2
+    spindleId = " " * 2
+    spare = " " * 4
+    header = mid + revision + noAckFlag + stationId + spindleId + spare
+    length = header.__len__
+    header_string_length = (4 - length) * " " + str(length)
+    header = header_string_length + header + message_end
+    message = ""
+
+    return message
+
+
+# communication start
+def MID0001():
+    return "00200001003         NUL"
+
+
+# communication stop
+def MID0003():
+    return "00200003            NUL"
+
+
+# get pset
+def MID0012():
+    return "00230012            001NUL"
+
+
 send("Hello Powerfocus!")
 
 connection = True
@@ -34,6 +71,9 @@ while connection:
     if user_input == "quit":
         connection = False
 
+        send(MID0003())
+
     send(user_input)
+
 
 send(DISCONNECT_MESSAGE)
